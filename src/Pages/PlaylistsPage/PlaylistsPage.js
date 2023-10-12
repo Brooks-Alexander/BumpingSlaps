0feb11ab3005dbaf657816dff3cb7c98613f9ecd
Daniel Dikiy <zer0charon@gmail.com>
2023-10-12 21:41:47 +0200

    fixes

--- src/Pages/PlaylistsPage/PlaylistsPage.js
+++ src/Pages/PlaylistsPage/PlaylistsPage.js
@@ -5,10 +5,14 @@ export default class PlaylistsPage extends React.Component {
   render() {
     return (
       <div className='PlaylistsPage'>
-        <h2>Your Playlists</h2>     
+        <h2>Your Playlists</h2>
         <input defaultValue={'New Playlist'} />
-        <Tracklist tracks={this.props.playlistTracks} />
-        <button ClassName='Playlist-save'>SAVE TO SPOTIFY</button>   
+        <Tracklist
+          tracks={this.props.playlistTracks}
+          onAdd={this.props.onAdd}
+          addSongToPlaylist={this.props.addSongToPlaylist}
+        />
+        <button className='Playlist-save'>SAVE TO SPOTIFY</button>
       </div>
     );
   }
